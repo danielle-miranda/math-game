563324fceec7386eeccbd53fab6fb244040feb06
Danielle Miranda <danielle.m.miranda96@gmail.com>
2024-12-02 13:44:54 -0800

    inhect equation into DOM

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 // index.js
-var randomNumberGenerator = function (size) {
-  return Math.ceil(Math.random() * size);
-}
+$(document).ready(function(){
 
-var questionGenerator = function () {
-  var question = {};
-  var num1 = randomNumberGenerator(10);
-  var num2 = randomNumberGenerator(10);
-  
-  question.answer = num1 + num2;
-  question.equation = String(num1) + " + " + String(num2);
-  
-  return question;
-}
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  }
 
-console.log(questionGenerator());
-console.log(questionGenerator());
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
+    
+    return question;
+  }
+
+  currentQuestion = questionGenerator();
+  $('#equation').text(currentQuestion.equation);
+});
